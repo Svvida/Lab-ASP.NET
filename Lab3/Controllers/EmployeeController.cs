9d61a3ecf956368dec5580993d60623ad6908537
Svvida <ksvvida@gmail.com>
2023-11-22 21:07:00 +0100

    Completed part of lab 3 exercises

---
+++ Lab3/Controllers/EmployeeController.cs
@@ -0,0 +1,127 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+     
+
+        private static List<EmployeeModel> employeeList = new List<EmployeeModel>
+{
+    new EmployeeModel { Id = 1, FirstName = "John", LastName = "Doe", PESEL = "12345678901", Position = "Developer", Department = "IT", HireDate = DateTime.Now, TerminationDate = null },
+    new EmployeeModel { Id = 2, FirstName = "Jane", LastName = "Smith", PESEL = "98765432109", Position = "Manager", Department = "HR", HireDate = DateTime.Now, TerminationDate = null },
+};
+
+        
+        public IActionResult Index()
+        {
+            return View(employeeList);
+        }
+
+        
+        public IActionResult Details(int id)
+        {
+            
+            var employee = employeeList.FirstOrDefault(e => e.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound(); 
+            }
+
+            return View(employee);
+        }
+
+        
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Form");
+        }
+
+       
+        [HttpPost]
+        public IActionResult Create(EmployeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = employeeList.Count + 1;
+                employeeList.Add(model);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", model);
+            }
+        }
+
+        
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var employee = employeeList.FirstOrDefault(e => e.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View("Edit", employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var index = employeeList.FindIndex(e => e.Id == model.Id);
+
+                if (index != -1)
+                {
+                    employeeList[index] = model;
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Edit", model);
+            }
+        }
+
+        
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            
+            var employee = employeeList.FirstOrDefault(e => e.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound(); 
+            }
+
+            return View(employee);
+        }
+
+       
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+          
+            var employee = employeeList.FirstOrDefault(e => e.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound(); 
+            }
+
+           
+            employeeList.Remove(employee);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Lab3/Models/EmployeeModel.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class EmployeeModel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The 'First Name' field is required.")]
+        [StringLength(50, ErrorMessage = "The 'First Name' field cannot be longer than 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The 'Last Name' field is required.")]
+        [StringLength(50, ErrorMessage = "The 'Last Name' field cannot be longer than 50 characters.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The 'PESEL' field is required.")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "The 'PESEL' field must consist of 11 digits.")]
+        public string PESEL { get; set; }
+
+        [Required(ErrorMessage = "The 'Position' field is required.")]
+        [StringLength(100, ErrorMessage = "The 'Position' field cannot be longer than 100 characters.")]
+        public string Position { get; set; }
+
+        [Required(ErrorMessage = "The 'Department' field is required.")]
+        [StringLength(50, ErrorMessage = "The 'Department' field cannot be longer than 50 characters.")]
+        public string Department { get; set; }
+
+        [Required(ErrorMessage = "The 'Hire Date' field is required.")]
+        [DataType(DataType.Date, ErrorMessage = "The 'Hire Date' field must be a date.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime HireDate { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "The 'Termination Date' field must be a date.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [ValidateTermination(ErrorMessage = "Termination Date must be later than Hire Date.")]
+        public DateTime? TerminationDate { get; set; }
+    }
+
+    public class ValidateTerminationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = (EmployeeModel)validationContext.ObjectInstance;
+
+            if (model.TerminationDate.HasValue && model.TerminationDate < model.HireDate)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
